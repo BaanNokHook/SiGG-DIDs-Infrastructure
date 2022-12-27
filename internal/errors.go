618431a3b54bb430fe21312992a6f2f23df94df9
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-27 16:02:14 +0700

    SiGG-DIDs-Infrastructure-Quater-internal-Errors

---
+++ internal/errors.go
@@ -0,0 +1,19 @@
+package internal
+
+import (
+	"fmt"
+)
+
+// These should probably closely match the HTTP status codes (though not a requirement),
+// as they're base types that will be used to determine the correct HTTP response code.
+var (
+	// ErrUnauthorized refers to the http response code unauthorized, which really means not authenticated, despite its name. See https://stackoverflow.com/a/6937030/155585
+	ErrUnauthorized = fmt.Errorf("unauthorized")
+	// ErrBadGateway means an invalid response was received from an upstream server (probably an OIDC provider)
+	ErrBadGateway = fmt.Errorf("bad gateway")
+
+	ErrNotFound    = fmt.Errorf("record not found")
+	ErrNotModified = fmt.Errorf("not modified")
+	ErrBadRequest  = fmt.Errorf("bad request")
+	ErrExpired     = fmt.Errorf("expired")
+)
